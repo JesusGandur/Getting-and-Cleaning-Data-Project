fc9c819ea64a4ffa69e89b2a072e821aae481746
Mibsen <m-ibsen@hotmail.com>
2014-04-17 21:00:18 +0200

    fixed an export bug, added some process info

--- run_analysis.R
+++ run_analysis.R
@@ -1,43 +1,38 @@
 
-run_analysis <- function (){
-
 file <- "data.zip"
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 data_path <- "UCI HAR Dataset"
 result_folder <- "results"
 
-##Install required packacets
-
+##Install required packacets  
 # looks if package is installed
 
 if(!is.element("plyr", installed.packages()[,1])){
+    print("Installing packages")
     install.packages("plyr")
 }
 
 library(plyr)
 
-
-
-## Create data and folders
-
+## Create data and folders   
 # verifies the data zip file has been downloaded
 if(!file.exists(file)){
     
     ##Downloads the data file
+    print("downloading Data")
     download.file(url,file, mode = "wb")
     
 }
 
 if(!file.exists(result_folder)){
+    print("Creating result folder")
     dir.create(result_folder)
-}
-
-
-
-
+} 
 
 ##reads a table from the zip data file and applies cols
 getTable <- function (filename,cols = NULL){
+    
+    print(paste("Getting table:", filename))
     
     f <- unz(file, paste(data_path,filename,sep="/"))
     
@@ -57,6 +52,8 @@ getTable <- function (filename,cols = NULL){
 ##Reads and creates a complete data set
 getData <- function(type, features){
     
+    print(paste("Getting data", type))
+    
     subject_data <- getTable(paste(type,"/","subject_",type,".txt",sep=""),"id")
     y_data <- getTable(paste(type,"/","y_",type,".txt",sep=""),"activity")    
     x_data <- getTable(paste(type,"/","X_",type,".txt",sep=""),features$V2) 
@@ -66,8 +63,11 @@ getData <- function(type, features){
 
 ##saves the data into the result folder
 saveResult <- function (data,name){
+    
+    print(paste("Saving data", name))
+    
     file <- paste(result_folder, "/", name,".csv" ,sep="")
-    write.csv(dataset1,file)
+    write.csv(data,file)
 }
 
 
@@ -109,6 +109,4 @@ colnames(dataset2[,-c(1:2)]) <- paste(colnames(dataset2[,-c(1:2)]), "_mean", sep
 
 saveResult(dataset2,"dataset2")
 
-}
 
-run_analysis()
